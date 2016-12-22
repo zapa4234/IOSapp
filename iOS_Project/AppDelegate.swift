05db58aa7dd7b744b4e299c178d5ebd948392db1
Jose Sanchez-Garcia <josebsan96@gmail.com>
2016-12-22 15:13:28 -0800

    Able to implement Twitter Log in for our app

--- iOS_Project/AppDelegate.swift
+++ iOS_Project/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Twitter.self])
         return true
     }
 
--- iOS_Project/ViewController.swift
+++ iOS_Project/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TwitterKit
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Implementing the Twitter Login for the app
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                                              message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
     }
 
     override func didReceiveMemoryWarning() {
